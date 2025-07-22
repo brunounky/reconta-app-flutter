f4232008a51e793a26b9e5859695c53003532e95
Bruno Campagnol <35274635+brunounky@users.noreply.github.com>
2025-07-22 20:32:51 -0300

    Update build.gradle.kts

--- reconta_app/android/build.gradle.kts
+++ reconta_app/android/build.gradle.kts
@@ -3,6 +3,15 @@ allprojects {
         google()
         mavenCentral()
     }
+
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.application") || project.plugins.hasPlugin("com.android.library")) {
+            val android = project.extensions.findByType(com.android.build.gradle.BaseExtension::class.java)
+            if (android != null) {
+                android.compileSdkVersion(34)
+            }
+        }
+    }
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
